ee121227b1bc1a4ce84e1cb6c92eb0f9525495cf
Unknown <gregoire.hage@gmail.com>
2020-10-31 01:41:15 +0100

    comment js & add delete in url parameters case

--- public/scripts/add-collection-widget.js
+++ public/scripts/add-collection-widget.js
@@ -4,90 +4,42 @@ $(document).ready(function () {
     let counter = $('#params-fields-list').children().length;
     let counterBody = $('#body-fields-list').children().length;
 
-    if (counter === 0) {
-        $('.rm-another-collection-widget').attr('disabled', '');
-    }
+    disableRmBtOnZeroCounter(counter, 'rm-another-collection-widget');
+    disableRmBtOnZeroCounter(counterBody, 'rm-another-collection-widget-body');
 
-    if (counterBody === 0) {
-        $('.rm-another-collection-widget-body').attr('disabled', '');
-    }
+    bodyOrParams();
 
-    if ($('#send_method').val() === 'GET') {
-        document.querySelector('#paramsdiv').classList.remove('d-none');
-        document.querySelector('#bodydiv').classList.add('d-none');
-    } else {
-        document.querySelector('#paramsdiv').classList.add('d-none');
-        document.querySelector('#bodydiv').classList.remove('d-none');
-    }
-
-    $('#send_url').on('focus', function (_e) {
+    $('#send_url').on('focus', function (_e) { // permet d'obtenir l'url précédente avant modification lors de l'entrée sur le champs
         if ($('#send_method').val() === 'GET') {
             previousUrl = this.value;
         }
     });
 
-    $('#send_method').on('change', function (_e) {
-        if (this.value === 'GET') {
-            document.querySelector('#paramsdiv').classList.remove('d-none');
-            document.querySelector('#bodydiv').classList.add('d-none');
-        } else {
-            document.querySelector('#paramsdiv').classList.add('d-none');
-            document.querySelector('#bodydiv').classList.remove('d-none');
-        }
+    $('#send_method').on('change', function (_e) { // permet de déterminer si ça utilise un body ou des query parameters lors du changement de la méthode
+        bodyOrParams();
     });
 
-    $('#send_url').on('change', function (e) {
+    $('#send_url').on('change', function (e) { // permet d'effectuer les modifications nécessaire des query parameters séparés lors du changement de l'url présente dans le champs
         if ($('#send_method').val() === 'GET') {
-            let urlParams = readUrl($('#send_url').val());
+            let url = $('#send_url').val();
+            let urlParams = readUrl(url);
             if (previousUrl !== '' && Object.keys(readUrl(previousUrl)).length === Object.keys(urlParams).length) {
-                for (let i = 0; i < Object.keys(urlParams).length; i++) {
-                    for (let j = 0; j < Object.keys(readUrl(previousUrl)).length; j++) {
-                        if (Object.keys(urlParams)[i] !== Object.keys(readUrl(previousUrl))[j]) {
-                            $('#params-fields-list').children().children('div').each(function () {
-                                if ($($($(this).children('div')[0]).children('input')[0]).val() === Object.keys(readUrl(previousUrl))[i]) {
-                                    $($($(this).children('div')[0]).children('input')[0]).val(Object.keys(urlParams)[i]);
-                                }
-                            });
-                        }
+                modifySeparatedParameters(urlParams);
+            } else if (previousUrl !== '' && Object.keys(readUrl(previousUrl)).length > Object.keys(urlParams).length) {
+                removeSeparatedParameters(url);
+            } else {
+                addToSeparatedParameters(urlParams);
+            }
 
-                        if(Object.values(urlParams)[i] !== Object.values(readUrl(previousUrl))[j]) {
-                            $('#params-fields-list').children().children('div').each(function () {
-                                if ($($($(this).children('div')[1]).children('input')[0]).val() === Object.values(readUrl(previousUrl))[i] && $($($(this).children('div')[0]).children('input')[0]).val() === Object.keys(urlParams)[i]) {
-                                    $($($(this).children('div')[1]).children('input')[0]).val(Object.values(urlParams)[i]);
-                                }
-                            });
-                        }
-                    }
-                }
-                return;
-            }
-            for (let i = 0; i < Object.keys(urlParams).length; i++) {
-                let exist = false;
-                $('#params-fields-list').children().children('div').each(function () {
-                    if ($($($(this).children('div')[0]).children('input')[0]).val() === Object.keys(urlParams)[i]) {
-                        $($($(this).children('div')[1]).children('input')[0]).val(Object.values(urlParams)[i]);
-                        exist = true;
-                    }
-                });
-                if (!exist) {
-                    $('.add-another-collection-widget').click();
-                    $($($($('#params-fields-list').children()).children().last().children('div')[0]).children('input')[0]).val(Object.keys(urlParams)[i]);
-                    $($($($('#params-fields-list').children()).children().last().children('div')[1]).children('input')[0]).val(Object.values(urlParams)[i]);
-                }
-            }
         }
     })
 
-    $($($('#params-fields-list').children()).children()).each(function () {
-        $($($(this).children('div')[0]).children('input')[0]).off('change').on('change', onChangeKey).off('focus').on('focus', onFocusKey);
-    });
+    bindEventsToKeys();
 
-    $($($('#params-fields-list').children()).children()).each(function () {
-        $($($(this).children('div')[1]).children('input')[0]).off('change').on('change', onChangeParam).off('focus').on('focus', onFocusParam);
-    });
+    bindEventsToParamsValues();
 
 
-    $('.add-another-collection-widget').click(function (e) {
+    $('.add-another-collection-widget').click(function (e) { // permet d'ajouter un query parameter supplémentaire lors du clique sur le bouton
         var list = $($(this).attr('data-list-selector'));
         // Try to find the counter of the list or use the length of the list
         var counter = list.data('widget-counter') || list.children().length;
@@ -107,16 +59,12 @@ $(document).ready(function () {
         var newElem = $(list.attr('data-widget-tags')).html(newWidget);
         newElem.appendTo(list);
         $('.rm-another-collection-widget').removeAttr('disabled');
-        $($($('#params-fields-list').children()).children()).each(function () {
-            $($($(this).children('div')[0]).children('input')[0]).off('change').on('change', onChangeKey).off('focus').on('focus', onFocusKey);
-        });
+        bindEventsToKeys();
 
-        $($($('#params-fields-list').children()).children()).each(function () {
-            $($($(this).children('div')[1]).children('input')[0]).off('change').on('change', onChangeParam).off('focus').on('focus', onFocusParam);
-        });
+        bindEventsToParamsValues();
     });
 
-    $('.add-another-collection-widget-body').click(function (e) {
+    $('.add-another-collection-widget-body').click(function (e) { // permet d'ajouter un body parameter supplémentaire lors du clique sur le bouton
         var list = $($(this).attr('data-list-selector'));
         // Try to find the counter of the list or use the length of the list
         var counter = list.data('widget-counter') || list.children().length;
@@ -138,7 +86,7 @@ $(document).ready(function () {
         $('.rm-another-collection-widget-body').removeAttr('disabled');
     });
 
-    $('.rm-another-collection-widget').click(function (e) {
+    $('.rm-another-collection-widget').click(function (e) { // permet d'enlever le dernier query parameter lors du clique sur le bouton
         var list = $($(this).attr('data-list-selector'));
         // Try to find the counter of the list or use the length of the list
         var counter = list.data('widget-counter') || list.children().length;
@@ -166,7 +114,7 @@ $(document).ready(function () {
 
     });
 
-    $('.rm-another-collection-widget-body').click(function (e) {
+    $('.rm-another-collection-widget-body').click(function (e) { // permet d'enlever le dernier body parameter lors du clique sur le bouton
         var list = $($(this).attr('data-list-selector'));
         // Try to find the counter of the list or use the length of the list
         var counter = list.data('widget-counter') || list.children().length;
@@ -192,7 +140,7 @@ $(document).ready(function () {
     });
 });
 
-function readUrl(url) {
+function readUrl(url) { // permet de lire une url et de renvoyer les paramètres sous forme d'objet
 
     var para_str = '';
 
@@ -235,7 +183,7 @@ function readUrl(url) {
 
 }
 
-function onChangeKey(_e) {
+function onChangeKey(_e) { // fonction permettant de faire les modifications nécessaire de la clé lors du changement de celle-ci
     if (($('#send_url').val()).includes(previousKey + '=') && previousKey !== '') {
         $('#send_url').val($('#send_url').val().replace(previousKey + '=', this.value + '='));
     } else {
@@ -249,17 +197,93 @@ function onChangeKey(_e) {
     }
 }
 
-function onChangeParam(_e) {
+function onChangeParam(_e) { // fonction permettant de faire les modifications nécessaire de la valeur du query parameter lors du changement de celle-ci
     if (($('#send_url').val()).includes($($($(this).parent().parent().children('div')[0]).children('input')[0]).val() + '=' + previousParam) && $($($(this).parent().parent().children('div')[0]).children('input')[0]).val() !== '') {
         $('#send_url').val($('#send_url').val().replace($($($(this).parent().parent().children('div')[0]).children('input')[0]).val() + '=' + previousParam, $($($(this).parent().parent().children('div')[0]).children('input')[0]).val() + '=' + this.value));
     }
 }
 
-function onFocusKey(_e) {
+function onFocusKey(_e) { // permet de définir la valeur précédente de la clé du query parameter
     previousKey = this.value;
 }
 
 
-function onFocusParam(_e) {
+function onFocusParam(_e) { // permet de définir la valeur précédente de la valeur du query parameterr
     previousParam = this.value;
+}
+
+function addToSeparatedParameters(urlParams){ // permet d'ajouter un ou plusieurs query parameters lors de la modification de l'url
+    for (let i = 0; i < Object.keys(urlParams).length; i++) {
+        let exist = false;
+        $('#params-fields-list').children().children('div').each(function () {
+            if ($($($(this).children('div')[0]).children('input')[0]).val() === Object.keys(urlParams)[i]) {
+                $($($(this).children('div')[1]).children('input')[0]).val(Object.values(urlParams)[i]);
+                exist = true;
+            }
+        });
+        if (!exist) {
+            $('.add-another-collection-widget').click();
+            $($($($('#params-fields-list').children()).children().last().children('div')[0]).children('input')[0]).val(Object.keys(urlParams)[i]);
+            $($($($('#params-fields-list').children()).children().last().children('div')[1]).children('input')[0]).val(Object.values(urlParams)[i]);
+        }
+    }
+}
+
+function modifySeparatedParameters(urlParams){ // permet de modifier les clés et/ou valeurs de un ou plusieurs query parameters lors de la modification de l'url
+    for (let i = 0; i < Object.keys(urlParams).length; i++) {
+        for (let j = 0; j < Object.keys(readUrl(previousUrl)).length; j++) {
+            if (Object.keys(urlParams)[i] !== Object.keys(readUrl(previousUrl))[j]) {
+                $('#params-fields-list').children().children('div').each(function () {
+                    if ($($($(this).children('div')[0]).children('input')[0]).val() === Object.keys(readUrl(previousUrl))[i]) {
+                        $($($(this).children('div')[0]).children('input')[0]).val(Object.keys(urlParams)[i]);
+                    }
+                });
+            }
+
+            if(Object.values(urlParams)[i] !== Object.values(readUrl(previousUrl))[j]) {
+                $('#params-fields-list').children().children('div').each(function () {
+                    if ($($($(this).children('div')[1]).children('input')[0]).val() === Object.values(readUrl(previousUrl))[i] && $($($(this).children('div')[0]).children('input')[0]).val() === Object.keys(urlParams)[i]) {
+                        $($($(this).children('div')[1]).children('input')[0]).val(Object.values(urlParams)[i]);
+                    }
+                });
+            }
+        }
+    }
+}
+
+function removeSeparatedParameters(url){ // permet de supprimer un ou plusieurs query parameters lors de la modification de l'url
+    for (let i = 0; i < Object.keys(readUrl(previousUrl)).length; i++){
+        $('.rm-another-collection-widget').click();
+    }
+    previousUrl = '';
+    addToSeparatedParameters(readUrl(url));
+    $('#send_url').val(url);
+}
+
+function bodyOrParams(){ // permet d'afficher soit la liste des query parameters ou soit le body selon la méthode choisi
+    if ($('#send_method').val() === 'GET') {
+        document.querySelector('#paramsdiv').classList.remove('d-none');
+        document.querySelector('#bodydiv').classList.add('d-none');
+    } else {
+        document.querySelector('#paramsdiv').classList.add('d-none');
+        document.querySelector('#bodydiv').classList.remove('d-none');
+    }
+}
+
+function disableRmBtOnZeroCounter(counter, btClass){ // permet de désactiver le bouton supprimer si il y a 0 paramètre dans la liste
+    if (counter === 0) {
+        $(`.${btClass}`).attr('disabled', '');
+    }
+}
+
+function bindEventsToKeys(){ // lie les événements nécessaire avec les différentes clés des query parameters
+    $($($('#params-fields-list').children()).children()).each(function () {
+        $($($(this).children('div')[0]).children('input')[0]).off('change').on('change', onChangeKey).off('focus').on('focus', onFocusKey);
+    });
+}
+
+function bindEventsToParamsValues(){ // lie les événements nécessaire avec les différentes valeurs des query parameters
+    $($($('#params-fields-list').children()).children()).each(function () {
+        $($($(this).children('div')[1]).children('input')[0]).off('change').on('change', onChangeParam).off('focus').on('focus', onFocusParam);
+    });
 }
